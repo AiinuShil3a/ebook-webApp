6774f49af900905bfba425283251b60ece169669
SuttiGuy <Guy.guy0205245@gmail.com>
2024-03-27 15:05:12 +0700

    Book_model

--- backend/Models/book.model.js
+++ backend/Models/book.model.js
@@ -1,13 +1,34 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 const BookSchema = new Schema({
-  bookname: { type: String},
-  author: { type: String},
-  image: { type: String},
-  publisher_url: { type: String},
-  publisher: { type: String},
-  isbn: { type: Number},
-
+  typeBook: {
+    type: String,
+    required: true,
+  },
+  nameBook: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  publisher_url: {
+    type: String,
+    required: true,
+  },
+  publisher: {
+    type: String,
+    required: true,
+  },
+  isbn: {
+    type: Number,
+    required: true,
+  },
 });
 const BookModel = model("Book", BookSchema);
 module.exports = BookModel;
